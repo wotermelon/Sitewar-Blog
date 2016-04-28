6e2d106c54cd80a3d60cfd2e3e10f8cd1f15ea46
zhangmingze <593244920@qq.com>
2016-04-28 16:24:01 +0800

    文章存档，文章标签完成

--- config/index.js
+++ config/index.js
@@ -4,10 +4,30 @@
 
 module.exports = { 
   PORT: 3000,
+
+  // mongo数据库配置
   mongo: {
     cookieSecret: 'mblog', 
     db: 'mblog', 
     host: '127.0.0.1',
     port: 27017
+  },
+  
+  // 邮箱配置
+  MAIL_OPTS: {
+      host: 'smtp.126.com',
+      port: 25,
+      auth: {
+          user: 'it_coolfish@126.com',
+          pass: '789512357',
+          name: '张铭泽'
+      }
+  },
+
+  // 管理员账号
+  admin: {
+    name: 'admin',
+    pwd: 'aaa',
+    head: "/img/header.png"
   }
 };
--- public/js/home.js
+++ public/js/home.js
@@ -1,21 +1,58 @@
 $(document).ready(function() {
-  laypage({
-    cont: 'z-pagination',
-    pages: $('#z-pagination').attr('allPages'),
-    skip: true,
-    skin: 'molv',
-    curr: $('#z-pagination').attr('page'),
-    first: 1,
-    last: $('#z-pagination').attr('allPages'),
-    jump: function (e, first) {
-      if(!first) {
-        var lostr = location.href;
-        if(lostr.indexOf("?") != -1){
-          location.href = lostr.replace(/\?p=\w+$/,"?p="+e.curr);
-        } else {
-          location.href = lostr + "?p=" +e.curr;
+  // 分页组件初始化
+  if($('#z-pagination').length != 0) {
+    laypage({
+      cont: 'z-pagination',
+      pages: $('#z-pagination').attr('allPages'),
+      skip: true,
+      skin: 'molv',
+      curr: $('#z-pagination').attr('page'),
+      first: 1,
+      last: $('#z-pagination').attr('allPages'),
+      jump: function (e, first) {
+        if(!first) {
+          var lostr = location.href;
+          if(lostr.indexOf("?") != -1){
+            location.href = lostr.replace(/\?p=\w+$/,"?p="+e.curr);
+          } else {
+            location.href = lostr + "?p=" +e.curr;
+          }
         }
       }
-    }
+    });
+  }
+
+  // 发表文章
+  $('.z-publish').on('click', function () {
+
+    var dataObj = {};
+    dataObj.title = $('#z-title').val();
+    dataObj.tags = $('#z-tag').val();
+    dataObj.post = $('#z-textarea').val();
+
+    $.ajax({
+      url: '/post',
+      dataType: 'JSON',
+      type: 'POST',
+      data: dataObj
+    }).done(function (result) {
+      if(result.success === true) {
+        alert(result.msg);
+      } else {
+        alert(result.arror);
+      }
+    });
+
   });
+
+  // 手风琴收缩
+  $('li[data-title]').on('click', function () {
+    var $this = $(this);
+    var $span = $this.find('span');
+    var clname = $this.attr("class");
+    console.log($span.get(0))
+    $this.siblings('.' + clname).toggle(200);
+    $span.html() === "放下"? $span.html('收起'):$span.html('放下');
+  })
+
 });
